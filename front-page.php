9b5012d072c7df5ea6fdcf8b8d3b44e206441e1e
yournewgoodname <damislav@net.hr>
2022-12-06 05:34:15 +0100

    added some page

---
+++ front-page.php
@@ -0,0 +1,14 @@
+<?php
+include('header.php'); ?>
+<div class="container">
+  <h1>
+    <?php the_title(); ?>
+  </h1>
+  <?php get_template_part('includes/section', 'content'); ?>
+</div>
+
+
+
+
+
+<?php include('footer.php') ?>
